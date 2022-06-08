b13b46d9c31775377a54badd3563b17a181c0ebd
Jayden Cowley <jcowley16@gmail.com>
2022-06-07 21:23:51 -0600

    Add Get route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,1 +1,19 @@
 const router = require('express').Router();
+const fs = require('fs')
+
+router.get('/notes', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) {
+            throw err;
+        }else {
+            let notes = [];
+            if (data) {
+                notes = JSON.parse(data)
+            }
+            return res.json(notes)
+        }
+    })
+})
+// post
+
+module.exports = router;
